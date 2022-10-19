a9532bb60762daac6338787dd361df548bd428a4
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2022-10-19 13:42:07 +0900

    add simple install script for bash

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# stop script when error(-e) or undefined(-u)
+set -eu
+
+# cd to the dir which contains this file
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+# exclusion file, dir list
+excludelist=(
+    ".git"
+)
+
+exflag=false
+# in dotfiles dir, search file whose name start with .
+for file in .??*; do
+    exflag=false
+    # exclude file check
+    for exfile in ${excludelist[@]}; do
+        if [[ $file == $exfile ]]; then
+            exflag=true
+            continue
+        fi
+    done
+
+    if [[ $exflag == false ]]; then
+        # make symbolic link, overwrite if already exists
+        ln -snfv ${PWD}/"$file" ~/"$file"
+    fi
+done
