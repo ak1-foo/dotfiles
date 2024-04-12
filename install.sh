ac79726faf7b6176a7dbfaa1a920081e0e44b122
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-04-13 01:28:45 +0900

    add install script for vscode in linux system

--- install.sh
+++ install.sh
@@ -38,3 +38,12 @@ if [ ! -d "$nvim_config_path" ]; then
     mkdir -p "$nvim_config_path"
 fi
 ln -snfv ${PWD}/init.vim $HOME/.config/nvim/init.vim
+
+# install vscode setting
+vscode_config_path="$HOME/.config/Code/User"
+# if there is no vscode config dir, make it
+if [ ! -d "$vscode_config_path" ]; then
+    mkdir -p "$vscode_config_path"
+fi
+ln -snfv "${PWD}"/.vscode/settings.json "$vscode_config_path"/settings.json
+ln -snfv "${PWD}"/.vscode/keybindings.json "$vscode_config_path"/keybindings.json
